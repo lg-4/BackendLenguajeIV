07b7b03a6688fef980ac9a55034a3c48ee9a924f
diegoespinal20re <diegoespinalp@gmail.com>
2024-07-07 23:21:39 -0600

    comentarios

--- DATABASE/modulo5/EstadisticasProcedure.sql
+++ DATABASE/modulo5/EstadisticasProcedure.sql
@@ -1,38 +1,39 @@
 
+-- PROCEDURE para insertar estadísticas
 CREATE PROCEDURE InsertEstadisticas(IN p_cod_est BIGINT, p_cod_veh BIGINT, nom_est VARCHAR(100), IN valor_est VARCHAR(100), fec_est DATE)
 BEGIN
-    START TRANSACTION;
-    INSERT INTO `ESTADISTICAS` (cod_estadistica, cod_vehiculo, nom_estadistica, valor, fec_estadistica) VALUES (p_cod_est, p_cod_veh, nom_est, valor_est, fec_est);
-    COMMIT;
+    START TRANSACTION; -- Inicia la transacción 
+    INSERT INTO `ESTADISTICAS` (cod_estadistica, cod_vehiculo, nom_estadistica, valor, fec_estadistica) VALUES (p_cod_est, p_cod_veh, nom_est, valor_est, fec_est); -- Inserta los valores proporcionados
+    COMMIT; -- Confirma la transacción
 END;
-CALL InsertEstadisticas(3, 1, 'MALO', '2', '2024-07-06');
+CALL InsertEstadisticas(3, 1, 'MALO', '2', '2024-07-06'); -- Ejecuta la inserción 
 
- 
+ -- PROCEDURE para actualizar estadísticas
 CREATE PROCEDURE UpdateEstadisticas(p_cod_veh BIGINT, nom_est VARCHAR(100), IN valor_est VARCHAR(100), fec_estadistica DATE, p_cod_est BIGINT)
     BEGIN
-    START TRANSACTION;
+    START TRANSACTION; -- Inicia la transacción 
         UPDATE `ESTADISTICAS` SET cod_vehiculo = p_cod_veh, 
                                     nom_estadistica = nom_est, 
                                     valor = valor_est, 
                                     fec_estadistica = fec_estadistica 
-        WHERE cod_estadistica = p_cod_est;
-    COMMIT;
+        WHERE cod_estadistica = p_cod_est; -- Actualiza los registros donde coincida el código de estadística
+    COMMIT; -- Confirma la transacción
     END; 
 
-CALL UpdateEstadisticas(1, 'EXELENTE', '10', '2024-07-06', 1); 
+CALL UpdateEstadisticas(1, 'EXELENTE', '10', '2024-07-06', 1); -- Ejecuta la actualización 
 
-
+-- PROCEDURE para seleccionar estadísticas
 CREATE PROCEDURE SelectEstadisticas()
 BEGIN
     SELECT 
-        v.cod_estadistica,
-        v.nom_estadistica,
-        v.valor,
-        v.fec_estadistica 
+        v.cod_estadistica, -- Código de la estadística
+        v.nom_estadistica, -- Nombre de la estadística
+        v.valor,           -- Valor de la estadística
+        v.fec_estadistica  -- Fecha de la estadística
     FROM ESTADISTICAS v 
-    INNER JOIN VEHICULOS a ON v.cod_vehiculo = a.cod_vehiculo
-    ORDER BY v.cod_estadistica;
+    INNER JOIN VEHICULOS a ON v.cod_vehiculo = a.cod_vehiculo -- Realiza un JOIN con la tabla VEHICULOS
+    ORDER BY v.cod_estadistica; -- Ordena los resultados por código de estadística
 END;
 
 
-CALL SelectEstadisticas();
+CALL SelectEstadisticas(); -- Ejecuta la selección de estadísticas
--- DATABASE/modulo5/EvaluacionesProcedure.sql
+++ DATABASE/modulo5/EvaluacionesProcedure.sql
@@ -1,39 +1,39 @@
-
+-- PROCEDURE para insertar evaluaciones
 CREATE PROCEDURE InsertEvaluaciones(IN p_cod_eva BIGINT, IN p_cod_veh BIGINT, IN resul_eva FLOAT, IN fec_eva DATE,IN p_cod_usuario BIGINT)
 BEGIN
-    START TRANSACTION;
-    INSERT INTO `EVALUACIONES` (cod_evaluacion, cod_vehiculo, resul_evaluacion, fec_evaluacion, cod_usuario) VALUES (p_cod_eva, p_cod_veh, resul_eva, fec_eva, p_cod_usuario);
-    COMMIT;
+    START TRANSACTION; -- Inicia la transacción 
+    INSERT INTO `EVALUACIONES` (cod_evaluacion, cod_vehiculo, resul_evaluacion, fec_evaluacion, cod_usuario) VALUES (p_cod_eva, p_cod_veh, resul_eva, fec_eva, p_cod_usuario); -- Inserta los valores proporcionados
+    COMMIT; -- Confirma la transacción
 END;
 
-CALL InsertEvaluaciones(2, 1, 7, '2024-07-07', 2);
+CALL InsertEvaluaciones(2, 1, 7, '2024-07-07', 2); -- Ejecuta la inserción 
 
-
+-- PROCEDURE para actualizar evaluaciones
 CREATE PROCEDURE UpdateEvaluaciones(IN p_cod_veh BIGINT, IN result_eva VARCHAR(50), IN fec_eva DATE, IN p_cod_usuario BIGINT, IN p_cod_eva BIGINT)
 BEGIN
-    START TRANSACTION;  
+    START TRANSACTION;  -- Inicia la transacción
     UPDATE `EVALUACIONES` SET cod_vehiculo = p_cod_veh,
                                 resul_evaluacion = result_eva,
                                 fec_evaluacion = fec_eva, 
                                 cod_usuario = p_cod_usuario
-    WHERE cod_evaluacion = p_cod_eva;
-    COMMIT;
+    WHERE cod_evaluacion = p_cod_eva; -- Actualiza los registros donde coincida el código de evaluación
+    COMMIT; -- Confirma la transacción
 END;
 
-CALL UpdateEvaluaciones(1, '8.5', '2024-07-07', 2, 2);
+CALL UpdateEvaluaciones(1, '8.5', '2024-07-07', 2, 2); -- Ejecuta la actualización de ejemplo
 
-
+-- PROCEDURE para seleccionar evaluaciones
 CREATE PROCEDURE SelectEvaluaciones()
 BEGIN
     SELECT 
-        v.cod_evaluacion,
-        v.resul_evaluacion,
-        v.fec_evaluacion,
-        v.cod_usuario 
+        v.cod_evaluacion,     -- Código de la evaluación
+        v.resul_evaluacion,   -- Resultado de la evaluación
+        v.fec_evaluacion,     -- Fecha de la evaluación
+        v.cod_usuario         -- Código del usuario que realizó la evaluación
     FROM EVALUACIONES v 
-    INNER JOIN VEHICULOS a ON v.cod_vehiculo = a.cod_vehiculo
-    INNER JOIN USUARIOS u ON v.cod_usuario = u.cod_usuario
-    ORDER BY v.cod_evaluacion;
+    INNER JOIN VEHICULOS a ON v.cod_vehiculo = a.cod_vehiculo -- Realiza un JOIN con la tabla VEHICULOS
+    INNER JOIN USUARIOS u ON v.cod_usuario = u.cod_usuario    -- Realiza un JOIN con la tabla USUARIOS
+    ORDER BY v.cod_evaluacion;                                -- Ordena los resultados por código de evaluación
 END;
 
-CALL SelectEvaluaciones();
+CALL SelectEvaluaciones(); -- Ejecuta la selección de evaluaciones
