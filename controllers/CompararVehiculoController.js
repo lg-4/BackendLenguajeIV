92eb54e0f159a5ff17f31e571d4c1a6997ef8ea3
CARDONA LUIS <91144533+CarLuis07@users.noreply.github.com>
2024-07-29 23:03:08 -0600

    Update CompararVehiculoController.js

--- controllers/CompararVehiculoController.js
+++ controllers/CompararVehiculoController.js
@@ -35,19 +35,21 @@ const postVehiculos = async (req, res) => {
 
 const putVehiculos = async (req, res) => {
     let veh = req.body;
-    var sql = "SET @cod_vehiculo = ?; SET @año_vehiculo = ?; SET @mot_vehiculo = ?; SET @pot_vehiculo = ?; SET @cod_marca = ?; SET @modelo = ?; SET @cod_tipo_transmision = ?;\
-    CALL UpdateVehiculos( cod_vehiculo,@año_vehiculo, @mot_vehiculo, @pot_vehiculo, @cod_marca, @modelo, @cod_tipo_transmision);";
+    var sql = "CALL UpdateVehiculos(?, ?, ?, ?, ?, ?, ?)";
 
-    mysqlConnection.query(sql, [veh.cod_vehiculo, veh.año_vehiculo, veh.mot_vehiculo, veh.pot_vehiculo, veh.cod_marca, veh.modelo, veh.cod_tipo_transmision], (err, rows, fields) => {
-        console.log("Valores recibidos:", veh);
-        if (!err)
-            res.send("Actualización Correcta");
-        else
-            console.log(err);
+    mysqlConnection.query(sql, [veh.cod_vehiculo, veh.año_vehiculo, veh.mot_vehiculo, veh.pot_vehiculo, veh.cod_marca, veh.modelo, veh.cod_tipo_transmision], (err, result, fields) => {
+        if (err) {
+            console.error('Error al ejecutar la consulta:', err);
+            res.status(500).json({ msg: 'Error al insertar el vehículo' });
+        } else {
+            res.json({
+                result
+            });
+        }
     });
 };
 
-//CALL UpdateVehiculos( 1,'2023','electri' , 'HOLA', 3, 'Corolla', 2);
+
 
 const getVehiculo= async (req, res) => {
     const params = [
